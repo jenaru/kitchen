62a6eaffe61a389ba43785c8b6aa5eed3be0ddb8
jenaru <jenaru.dev@gmail.com>
2024-08-14 17:02:51 +0200

    Cambiando url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Configuración de Axios
 const api = axios.create({
-  baseURL: "http://localhost:5000", // URL del backend
+  baseURL: "https://kitchen-backend-7efv.onrender.com", // URL del backend
   withCredentials: true, // Si necesitas enviar cookies o encabezados de autenticación
 });
 
