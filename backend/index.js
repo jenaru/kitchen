a24be3fac1221475b62f65a4ed14fcb5750af0f9
jenaru <jenaru.dev@gmail.com>
2024-08-14 17:16:27 +0200

    Configurando CORS

--- backend/index.js
+++ backend/index.js
@@ -13,27 +13,21 @@ console.log("PORT", port);
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://kitchenck-frontend.vercel.app/",
+  "https://kitchenck-frontend-jenarus-projects.vercel.app",
+  "https://kitchenck-frontend-jenaru-jenarus-projects.vercel.app",
   "http://127.0.0.1:5173",
 ];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Permite solicitudes sin origen (como aplicaciones móviles o curl)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     optionsSuccessStatus: 200,
   })
 );
+
+app.options("*", cors()); // Asegura que las solicitudes OPTIONS sean manejadas correctamente
 
 app.use(express.json());
 
